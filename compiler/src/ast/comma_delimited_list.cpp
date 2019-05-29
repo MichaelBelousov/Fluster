ee1be28d4d00127b97001f05fc2ad7bb27f4913a
Michael Belousov <michael.belousov98@gmail.com>
2019-05-29 13:26:45 -0400

    added CommaDelimitedList class

---
+++ compiler/src/ast/comma_delimited_list.cpp
@@ -0,0 +1,37 @@
+#include <vector>
+#include <memory>
+#include "comma_delimited_list.h"
+
+namespace fluster { namespace ast {
+
+
+
+//// Methods
+
+void
+CommaDelimitedList::
+append(Node::Ptr in);
+
+//// Construction
+
+static
+Block::Ptr
+CommaDelimitedList::
+empty()
+{
+    return CommaDelimitedList();
+}
+
+static
+CommaDelimitedList::Ptr
+CommaDelimitedList::
+fromFirst(Node::Ptr first)
+{
+    auto result = CommaDelimitedList();
+    result.emplace_back(first);
+    return result;
+}
+
+
+
+}; };
---
+++ compiler/src/ast/comma_delimited_list.h
@@ -0,0 +1,41 @@
+#ifndef FLUSTER_COMPILER_AST_COMMA_DELIMITED_LIST
+#define FLUSTER_COMPILER_AST_COMMA_DELIMITED_LIST
+
+#include <vector>
+#include <memory>
+#include "node.h"
+
+namespace fluster { namespace ast {
+
+
+
+struct CommaDelimitedList final
+    : public Node
+{
+    //// Methods
+    void
+    append(Node::Ptr in);
+
+    //// Construction
+    static
+    Block::Ptr
+    empty();
+
+    static
+    CommaDelimitedList::Ptr
+    fromFirst(Node::Ptr first);
+
+    //// Types
+    using Ptr = std::shared_ptr<CommaList>;
+
+private:
+
+    //// Members
+    std::vector<Node::Ptr> elements;
+};
+
+
+
+}; };
+
+#endif //FLUSTER_COMPILER_AST_COMMA_DELIMITED_LIST
