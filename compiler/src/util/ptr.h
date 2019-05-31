9ec2f8823044b37dca99f4bd793532dcd848ccc6
Michael Belousov <michael.belousov98@gmail.com>
2019-05-31 12:56:39 -0400

    experimental move of Ptr behavior to be well... like an actual C++ pointer

--- compiler/src/util/ptr.h
+++ compiler/src/util/ptr.h
@@ -14,73 +14,48 @@ struct Ptr : std::shared_ptr<T>
     //the other instantiations' members. This is necessary for construction
     template<typename U> template<typename V> friend Ptr<V> Ptr<U>::castUp();
     template<typename U> template<typename V> friend Ptr<V> Ptr<U>::castDown();
+
+    //// Construction
 private:
     struct SpecialConstructorTag {};
-    // NOTE: NullConstructorTag will be optimized out by the compiler since it
+    // NOTE: SpecialConstructorTag will be optimized out by the compiler since it
     // is an unused empty argument, this allows us to use overload resoltion
     // to choose between constructing an underlying object and returning a pointer
     // or constructing a smart pointer directly
     template<typename ...Args>
     Ptr(SpecialConstructorTag _, Args&& ...args)
         : std::shared_ptr<T>(
-            std::forward<Args>(args)...)
-    {}
-    //template<typename U = void, typename = std::enable_if_t<std::is_same<T,U>::value>>
-    Ptr(SpecialConstructorTag _, std::shared_ptr<T> ptr)
-        : std::shared_ptr<T>(std::forward<std::shared_ptr<T>>(ptr))
+            std::make_shared<T>(
+                std::forward<Args>(args)...))
     {}
 
 public:
     template<typename ...Args>
     Ptr(Args&& ...args)
         : std::shared_ptr<T>(
-            std::make_shared<T>(
-                std::forward<Args>(args)...))
+            std::forward<Args>(args)...)
     {}
 
     //// Methods
-    static
-    Ptr<T>
-    null()
-    {
-        return Ptr(SpecialConstructorTag());
-    }
-
     template<typename U>
     Ptr<U>
     castUp()
     {
-        return Ptr<U> (
-            typename Ptr<U>::SpecialConstructorTag(),
-            std::static_pointer_cast<U>(*this)
-        );
+        return Ptr<U>(std::static_pointer_cast<U>(*this));
     }
 
     template<typename U>
     Ptr<U>
     castDown()
     {
-        return Ptr<U> (
-            SpecialConstructorTag(),
-            std::dynamic_pointer_cast<U>(*this)
-        );
+        return Ptr<U>(std::dynamic_pointer_cast<U>(*this));
     }
 
     template<typename ...Args>
-    static
     Ptr<T>
-    copy(Args&& ...args)
+    make(Args&& ...args)
     {
-        return Ptr(SpecialConstructorTag(),
-                   std::forward<Args>(args)...);
-    }
-
-    template<typename U = void, typename = std::enable_if_t<std::is_same<T,U>::value>>
-    static
-    Ptr<U>
-    copy(Ptr<U>&& in)
-    {
-        return Ptr(SpecialConstructorTag(), std::forward<Ptr<T>>(in));
+        return Ptr(SpecialConstructorTag(), std::forward<Args>(args)...);
     }
 };
 
